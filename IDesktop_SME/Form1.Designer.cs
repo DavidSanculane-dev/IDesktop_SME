117110e82b333e029ec521e1d8a51aa0a8f18f35
DavidSanculane-dev <sanculane.dev@gmail.com>
2022-01-15 14:03:32 +0200

    Adicionando função webcam

--- IDesktop_SME/Form1.Designer.cs
+++ IDesktop_SME/Form1.Designer.cs
@@ -37,12 +37,28 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblHora = new System.Windows.Forms.Label();
             this.lblData = new System.Windows.Forms.Label();
+            this.pnlBrowser = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.picCamera = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pnlVertical.SuspendLayout();
             this.panelHorizontal.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlVertical
             // 
             this.pnlVertical.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.pnlVertical.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlVertical.Controls.Add(this.panel3);
+            this.pnlVertical.Controls.Add(this.panel2);
+            this.pnlVertical.Controls.Add(this.panel1);
             this.pnlVertical.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlVertical.Location = new System.Drawing.Point(0, 0);
             this.pnlVertical.Name = "pnlVertical";
@@ -115,18 +131,87 @@
             this.lblData.BackColor = System.Drawing.Color.Transparent;
             this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblData.ForeColor = System.Drawing.Color.White;
-            this.lblData.Location = new System.Drawing.Point(202, 35);
+            this.lblData.Location = new System.Drawing.Point(174, 35);
             this.lblData.Name = "lblData";
             this.lblData.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.lblData.Size = new System.Drawing.Size(57, 20);
             this.lblData.TabIndex = 6;
             this.lblData.Text = "Label2";
             // 
+            // pnlBrowser
+            // 
+            this.pnlBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBrowser.Location = new System.Drawing.Point(182, 60);
+            this.pnlBrowser.Name = "pnlBrowser";
+            this.pnlBrowser.Size = new System.Drawing.Size(821, 467);
+            this.pnlBrowser.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.picCamera);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 348);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 177);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 89);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(180, 259);
+            this.panel3.TabIndex = 2;
+            // 
+            // picCamera
+            // 
+            this.picCamera.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.picCamera.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.picCamera.Location = new System.Drawing.Point(0, 39);
+            this.picCamera.Name = "picCamera";
+            this.picCamera.Size = new System.Drawing.Size(180, 138);
+            this.picCamera.TabIndex = 0;
+            this.picCamera.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(68, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(65, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 39);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 527);
+            this.Controls.Add(this.pnlBrowser);
             this.Controls.Add(this.panelHorizontal);
             this.Controls.Add(this.pnlVertical);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -134,8 +219,13 @@
             this.Text = "iDestkop_SME";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.pnlVertical.ResumeLayout(false);
             this.panelHorizontal.ResumeLayout(false);
             this.panelHorizontal.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -149,6 +239,13 @@
         private System.Windows.Forms.Timer timer1;
         internal System.Windows.Forms.Label lblHora;
         internal System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Panel pnlBrowser;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picCamera;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- IDesktop_SME/Form1.cs
+++ IDesktop_SME/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace IDesktop_SME
 {
     public partial class frmPrincipal : Form
     {
+        public ChromiumWebBrowser browser;
+
+        public void IniciarNavegador()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://sistemaprontos.com.br/auth/realms/motaengil/protocol/openid-connect/auth?client_id=portal-web&redirect_uri=https%3A%2F%2Fmotaengil-portal.sistemaprontos.com.br%2F%23%2Fdashboard&state=2ec193c0-b955-4d3e-8a4c-1dd11763242a&response_mode=fragment&response_type=code&scope=openid&nonce=da178c79-e53e-4791-9213-9f54c98817a7#");
+            this.pnlBrowser.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+
+
+
         public frmPrincipal()
         {
             InitializeComponent();
+            IniciarNavegador();
         }
 
         private void btClose_Click(object sender, EventArgs e)
@@ -24,15 +39,15 @@ namespace IDesktop_SME
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            //lblData.Text = DateTime.Now.ToShortDateString();
-            //lblHora.Text = DateTime.Now.ToShortTimeString();
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //DateTime dt = new DateTime();
-            lblData.Text = DateTime.Now.ToShortDateString();
-            lblHora.Text = DateTime.Now.ToLongTimeString();
+
+            //blData.Text = DateTime.Now.ToShortDateString();
+            lblHora.ForeColor = Color.White;
+            lblHora.Text = DateTime.Now.ToLongTimeString() + " <> " + DateTime.Now.ToShortDateString();
         }
     }
 }
